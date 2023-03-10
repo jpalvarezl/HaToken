d2972b2a7069cac12426a99737625dd68b936972
Jose Alvarez <josealvar@microsoft.com>
2023-03-10 15:49:08 -0800

    wip

--- constants/Encodings.cs
+++ constants/Encodings.cs
@@ -1,52 +1,54 @@
+using LanguageModel;
+
 namespace Encoding;
 
 internal static class Utils {
-    public static string EncodingFor(string model) => model switch {
+    public static ModelName EncodingFor(string model) => model switch {
         // chat
         string x when x.StartsWith("gpt-3.5-turbo-")
-            || x == "gpt-3.5-turbo" => "cl100k_base",
+            || x == "gpt-3.5-turbo" => ModelName.cl100k_base,
 
         // text
-        "text-davinci-003" => "p50k_base",
-        "text-davinci-002" => "p50k_base",
-        "text-davinci-001" => "r50k_base",
-        "text-curie-001" => "r50k_base",
-        "text-babbage-001" => "r50k_base",
-        "text-ada-001" => "r50k_base",
-        "davinci" => "r50k_base",
-        "curie" => "r50k_base",
-        "babbage" => "r50k_base",
-        "ada" => "r50k_base",
+        "text-davinci-003" => ModelName.p50k_base,
+        "text-davinci-002" => ModelName.p50k_base,
+        "text-davinci-001" => ModelName.r50k_base,
+        "text-curie-001" => ModelName.r50k_base,
+        "text-babbage-001" => ModelName.r50k_base,
+        "text-ada-001" => ModelName.r50k_base,
+        "davinci" => ModelName.r50k_base,
+        "curie" => ModelName.r50k_base,
+        "babbage" => ModelName.r50k_base,
+        "ada" => ModelName.r50k_base,
 
         // code
-        "code-davinci-002" => "p50k_base",
-        "code-davinci-001" => "p50k_base",
-        "code-cushman-002" => "p50k_base",
-        "code-cushman-001" => "p50k_base",
-        "davinci-codex" => "p50k_base",
-        "cushman-codex" => "p50k_base",
+        "code-davinci-002" => ModelName.p50k_base,
+        "code-davinci-001" => ModelName.p50k_base,
+        "code-cushman-002" => ModelName.p50k_base,
+        "code-cushman-001" => ModelName.p50k_base,
+        "davinci-codex" => ModelName.p50k_base,
+        "cushman-codex" => ModelName.p50k_base,
 
         // edit
-        "text-davinci-edit-001" => "p50k_edit",
-        "code-davinci-edit-001" => "p50k_edit",
+        "text-davinci-edit-001" => ModelName.p50k_edit,
+        "code-davinci-edit-001" => ModelName.p50k_edit,
 
         // embeddings
-        "text-embedding-ada-002" => "cl100k_base",
+        "text-embedding-ada-002" => ModelName.cl100k_base,
 
         // old embeddings
-        "text-similarity-davinci-001" => "r50k_base",
-        "text-similarity-curie-001" => "r50k_base",
-        "text-similarity-babbage-001" => "r50k_base",
-        "text-similarity-ada-001" => "r50k_base",
-        "text-search-davinci-doc-001" => "r50k_base",
-        "text-search-curie-doc-001" => "r50k_base",
-        "text-search-babbage-doc-001" => "r50k_base",
-        "text-search-ada-doc-001" => "r50k_base",
-        "code-search-babbage-code-001" => "r50k_base",
-        "code-search-ada-code-001" => "r50k_base",
+        "text-similarity-davinci-001" => ModelName.r50k_base,
+        "text-similarity-curie-001" => ModelName.r50k_base,
+        "text-similarity-babbage-001" => ModelName.r50k_base,
+        "text-similarity-ada-001" => ModelName.r50k_base,
+        "text-search-davinci-doc-001" => ModelName.r50k_base,
+        "text-search-curie-doc-001" => ModelName.r50k_base,
+        "text-search-babbage-doc-001" => ModelName.r50k_base,
+        "text-search-ada-doc-001" => ModelName.r50k_base,
+        "code-search-babbage-code-001" => ModelName.r50k_base,
+        "code-search-ada-code-001" => ModelName.r50k_base,
 
         // open source
-        "gpt2" => "gpt2",
+        "gpt2" => ModelName.gpt2,
 
         _ => throw new ArgumentException("Unsupported model")
     };
