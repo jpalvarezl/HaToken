adbc2fa4a7e075f51a753a69b30a341af99946a1
Jose Alvarez <josealvar@microsoft.com>
2023-04-13 13:23:27 -0700

    Clarified main

--- Program.cs
+++ Program.cs
@@ -4,8 +4,9 @@ using Encoders.AML;
 internal class Program {
 
     public static async Task Main(string[] args) {
-        var tokenIds = await AzureMLEncoder.Encode("Hello world", EncoderName.gpt2);
-        // var tokenIds = await NonAzureEncoder.Encode("Hello world", Utils.EncodingFor("text-davinci-003"));
+        // "text-davinci-003" is the encoder for GTP3
+        // var tokenIds = await AzureMLEncoder.Encode("Hello world", Utils.EncodingFor("text-davinci-003"));
+        var tokenIds = await NonAzureEncoder.Encode("Hello world", Utils.EncodingFor("text-davinci-003"));
 
         foreach(var tokenId in tokenIds) {
             Console.WriteLine(tokenId);
