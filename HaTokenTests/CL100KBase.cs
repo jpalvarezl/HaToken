2b79cd5130614c71db0b9201c933280d361e6298
Jose Alvarez <josealvar@microsoft.com>
2023-06-19 22:33:30 +0000

    Switched over to xUnit

--- HaTokenTests/CL100KBase.cs
+++ HaTokenTests/CL100KBase.cs
@@ -1,17 +1,16 @@
-
 using Encoders;
+using Xunit;
 
 namespace HaTokenTests;
 
-[TestClass]
 public class CL100KBase
 {
-    [TestMethod]
+    [Fact]
     public async Task HelloWorld()
     {
         List<int> actual = await NonAzureEncoder.Encode("Hello world", Utils.EncodingFor("gpt-3.5-turbo-"));
         List<int> expected = new List<int>() { 9906, 1917 };
 
-        Assert.IsTrue(expected.SequenceEqual(actual));
+        Assert.True(expected.SequenceEqual(actual));
     }
 }
--- test-tools/generate_test_data.py
+++ test-tools/generate_test_data.py
@@ -11,15 +11,21 @@ import sys
 # [{ "text": "...", tokens: [ 1, ... ] }, ...]
 
 
-if(len(sys.argv) < 3):
-    print("Invalid number of arguments")
-    print("Usage: ./generate_test_data.py <input_file> <model_name>")
-    print("Example: ./generate_test_data.py input_text_only.json gpt2")
-    exit(1)
+# TODO: uncomment if we ever need this parameterized
+# if(len(sys.argv) < 3):
+#     print("Invalid number of arguments")
+#     print("Usage: ./generate_test_data.py <input_file> <model_name>")
+#     print("Example: ./generate_test_data.py input_text_only.json gpt2")
+#     exit(1)
 
-file_name = sys.argv[1]
-encoder_name = sys.argv[2]
+# file_name = sys.argv[1]
+# encoder_name = sys.argv[2]
+# End Of TODO
 
+# Assumption: You are executing the script from the folder it's contained
+
+file_name = "input_text_only.json"
+encoder_name = "p50k_base"
 
 with open(file_name) as f:
     data = json.load(f)
@@ -32,5 +38,5 @@ for text in data:
     result = {"text": text, "tokens": tokens}
     results.append(result)
 
-with open('test_data_{}.json'.format(encoder_name), 'w') as output_file:
+with open('../HaTokenTests/test_data/{}.json'.format(encoder_name), 'w') as output_file:
     json.dump(results, output_file)
